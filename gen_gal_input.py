a47c4eff728a5e313e9eaaf3444ac7224059e2a5
Aritra Ghosh <aritraghsh09@gmail.com>
2018-04-02 18:37:07 -0400

    Incorporate Multiprocessing

--- gen_gal_input.py
+++ gen_gal_input.py
@@ -6,26 +6,12 @@
 #  A python script to generate GalSim Template files
 #############################################
 
+from multiprocessing import Pool
 import numpy as np
 
-NUM_ITER = 2
-
-#Draw the parameters for the sersic object from appropriate distributions
-sersic_idx = np.random.uniform(0.5, 10, NUM_ITER)
-half_light_radius = np.random.uniform(2.0,9.0,NUM_ITER)
-axis_ratio = np.random.uniform(0.1,0.9,NUM_ITER)
-position_angle = np.random.uniform(-90.0,90.0,NUM_ITER)
-
-#store all generated parameter values corresponding to the images in a file
-para_file = open("sim_para.txt","w") ###MAKE THIS .GZ later if size is a problem###
-stacked_para = np.column_stack((sersic_idx,half_light_radius,axis_ratio,position_angle))
-np.savetxt(para_file,stacked_para,delimiter=" ",header="sersic_idx R_e axis_ratio PA",fmt="%.4f")
-para_file.close()
-
-print("Parameters generated....Creating Text files\n")
-
-for i in range(0,NUM_ITER):
 	
+def file_write(i):
+
 	template_file = open('galfit_temp_'+str(i),'w')
 
 	template_file.write("===============================================================================\n")
@@ -38,16 +24,16 @@ for i in range(0,NUM_ITER):
 	template_file.write("E) 1\n") #PSF fine sampling factor relative to data
 	template_file.write("F) none\n")
 	template_file.write("G) none\n")
-	template_file.write("H) 1 93 1 93\n") #Image region to fit (xmin xmax ymin ymax)
-	template_file.write("I) 100 100\n") #Size of the convolution box (x y)
+	template_file.write("H) 1 200 1 200\n") #Image region to fit (xmin xmax ymin ymax)
+	template_file.write("I) 200 200\n") #Size of the convolution box (x y)
 	template_file.write("J) 26.563\n") # Magnitude photometric zeropoint 
-	template_file.write("K) 0.038 0.038\n") # Magnitude photometric zeropoint 
+	template_file.write("K) 0.038 0.038\n")# Plate scale (dx dy)    [arcsec per pixel] 
 	template_file.write("O) regular\n") # Display type (regular, curses, both)
 	template_file.write("P) 1\n\n") # Choose: 0=optimize, 1=model, 2=imgblock, 3=subcomps
 	
 	#Object number 1
 	template_file.write(" 0) sersic\n") #object type
-	template_file.write(" 1) 48.5180 51.2800 0 0\n")#position x y
+	template_file.write(" 1) 100.0 100.0 0 0\n")#position x y
 	template_file.write(" 3) 20.0890 0\n")#Integrated Magnitude
 	template_file.write(" 4) "+str(half_light_radius[i])+" 0\n")#R_e (half-light radius)   [pix]
 	template_file.write(" 5) "+str(sersic_idx[i])+" 0\n")#Sersic index n (de Vaucouleurs n=4)
@@ -67,3 +53,26 @@ for i in range(0,NUM_ITER):
 
 	template_file.write("===============================================================================")
 	template_file.close()
+
+if __name__ == '__main__':
+	
+	NUM_ITER = 2
+	NUM_THREADS = 2	
+
+	#Draw the parameters for the sersic object from appropriate distributions
+	sersic_idx = np.random.uniform(0.5, 10, NUM_ITER)
+	half_light_radius = np.random.uniform(1.0,4.0,NUM_ITER)
+	axis_ratio = np.random.uniform(0.2,0.8,NUM_ITER)
+	position_angle = np.random.uniform(-90.0,90.0,NUM_ITER)
+
+	#store all generated parameter values corresponding to the images in a file
+	para_file = open("sim_para.txt","w") ###MAKE THIS .GZ later if size is a problem###
+	stacked_para = np.column_stack((sersic_idx,half_light_radius,axis_ratio,position_angle))
+	np.savetxt(para_file,stacked_para,delimiter=" ",header="sersic_idx R_e axis_ratio PA",fmt="%.4f")
+	para_file.close()
+
+	print("Parameters generated....Creating Text files\n")
+
+	pl = Pool(NUM_THREADS)
+	pl.map(file_write,range(0,NUM_ITER))
+
