aa30df4207fd029f6280eb89e3521fe5834ad3f0
Aritra Ghosh <aritraghsh09@gmail.com>
2018-04-02 16:46:34 -0400

    added feature to draw from distrbutions and store generated parameters to a parameter file

--- gen_gal_input.py
+++ gen_gal_input.py
@@ -6,7 +6,24 @@
 #  A python script to generate GalSim Template files
 #############################################
 
-for i in range(1,3):
+import numpy as np
+
+NUM_ITER = 2
+
+#Draw the parameters for the sersic object from appropriate distributions
+sersic_idx = np.random.uniform(0.5, 10, NUM_ITER)
+half_light_radius = np.random.uniform(2.0,9.0,NUM_ITER)
+axis_ratio = np.random.uniform(0.1,0.9,NUM_ITER)
+position_angle = np.random.uniform(-90.0,90.0,NUM_ITER)
+
+#store all generated parameter values corresponding to the images in a file
+para_file = open("sim_para.txt","w") ###MAKE THIS .GZ later if size is a problem###
+stacked_para = np.column_stack((sersic_idx,half_light_radius,axis_ratio,position_angle))
+np.savetxt(para_file,stacked_para,delimiter=" ",header="sersic_idx R_e axis_ratio PA",fmt="%.4f")
+para_file.close()
+
+
+for i in range(0,NUM_ITER):
 	
 	template_file = open('galfit_temp_'+str(i),'w')
 
@@ -31,13 +48,13 @@ for i in range(1,3):
 	template_file.write(" 0) sersic\n") #object type
 	template_file.write(" 1) 48.5180 51.2800 0 0\n")#position x y
 	template_file.write(" 3) 20.0890 0\n")#Integrated Magnitude
-	template_file.write(" 4) 5.1160 0\n")#R_e (half-light radius)   [pix]
-	template_file.write(" 5) 4.2490 0\n")#Sersic index n (de Vaucouleurs n=4)
+	template_file.write(" 4) "+str(half_light_radius[i])+" 0\n")#R_e (half-light radius)   [pix]
+	template_file.write(" 5) "+str(sersic_idx[i])+" 0\n")#Sersic index n (de Vaucouleurs n=4)
 	template_file.write(" 6) 0.0000 0\n") 
 	template_file.write(" 7) 0.0000 0\n")
 	template_file.write(" 8) 0.0000 0\n")
-	template_file.write(" 9) 0.7570 0\n")#axis ratio (b/a) 
-	template_file.write("10) -60.3690 0\n")#position angle (PA) [deg: Up=0, Left=90]
+	template_file.write(" 9) "+str(axis_ratio[i])+" 0\n")#axis ratio (b/a) 
+	template_file.write("10) "+str(position_angle[i])+" 0\n")#position angle (PA) [deg: Up=0, Left=90]
 	template_file.write(" Z) 0\n\n")#output option (0 = resid., 1 = Don't subtract) 
 
 	#Object number 2
