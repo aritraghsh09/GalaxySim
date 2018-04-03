7232eb4ad6f9d153b2b50b18025883da01264756
Aritra Ghosh <aritraghsh09@gmail.com>
2018-04-03 11:44:26 -0400

    Added Time Tracking and pickling of exit codes in case all are not zero

--- run_galfit.py
+++ run_galfit.py
@@ -9,21 +9,36 @@
 from multiprocessing import Pool 
 import os
 import numpy as np
+import time
+import pickle
 
-NUM_THREADS = 15
-NUM_FILES = 100000
-FILE_PATH = "/net/urry/ag2422/gal_sim_files_0/"
+NUM_FILES = 2
+NUM_THREADS = 1
+#FILE_PATH = "/net/urry/ag2422/gal_sim_files_0/"
+FILE_PATH = "./"
 
 def run_galfit(x):
-	command = "~/local_soft/galfit "+FILE_PATH+"galfit_temp_" + str(x) + " > /dev/null 2>&1"
+	command = "~/local_soft/galfit "+FILE_PATH+"galfit_temp_" + str(x) + " > /dev/null 2>&1" #Suppresses Screen I/O. Remove the end-part if you don't want this. 
 	out = os.system(command)
 	return out
 
 
 if __name__ == '__main__':
 
+	start_timestamp = time.time()
+	
 	pl = Pool(NUM_THREADS)
 	exit_codes = pl.map(run_galfit,range(0,NUM_FILES))
 
 	bool = all(i == 0 for i in exit_codes)
-	print bool #if FALSE is printed. Somethin went wrong. Check
+	
+	if bool == True:
+		print("All seems to have gone well!!")
+	else:
+		print("Some Images may have some issues")
+		pickle_file = open("exit_codes.pkl","w")
+		pickle.dump(exit_codes,pickle_file)
+		print("Dumped Array of Exit Codes as a pickle.Check for non-zero exit codes.")		
+
+
+	print("Real Time of Execution:- %s seconds" % (time.time() - start_timestamp) )
