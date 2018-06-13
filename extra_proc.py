8d58800cc358e7497b92bf5b5089ca72fa2643e4
Aritra Ghosh <aritraghsh09@gmail.com>
2018-06-13 01:07:37 -0400

    Incorporated Noise Addition Feature

--- extra_proc.py
+++ extra_proc.py
@@ -14,29 +14,36 @@ from multiprocessing import Pool
 from astropy.convolution import convolve
 
 dataReadPath = '/net/urry/ag2422/gal_sim_runs/gal_sim_images_6/'
-dataWritePath = '/net/urry/ag2422/gal_sim_runs/gal_sim_images_6_convolved/'
+dataWritePath = '/net/urry/ag2422/gal_sim_runs/gal_sim_images_6_extra_proc_i/'
+psfFilePath = "/net/urry/ag2422/powell_17_data/GOODSN_ACS_cutouts/goodss_3dhst.v4.0.F775W_psf.fits"
+noiseFilePath = '/net/urry/ag2422/noise_images/noise_si.npy'
 
 NUM_FILES_TOTAL = 100000
 NUM_THREADS = 15
+IMG_WIDTH = IMG_HEIGHT = 167 #No of pixels length/width -wise in each cutout
 
-def convolve_psf(i):
+noise = np.load(noiseFilePath)
+
+def psf_and_noise(i):
 	img_data = fits.getdata(dataReadPath+"output_img_"+str(i)+".fits",memmap=False)
-	psf_data = fits.getdata("/net/urry/ag2422/powell_17_data/GOODSN_ACS_cutouts/goodss_3dhst.v4.0.F775W_psf.fits",memmap=False)
+	psf_data = fits.getdata(psfFilePath, memmap=False)
 	convolved_data = convolve(img_data,psf_data)
-	
-	hdu = fits.PrimaryHDU(convolved_data)
+	final_data = convolved_data + np.random.choice(noise,size=(IMG_WIDTH,IMG_HEIGHT))	
+
+	hdu = fits.PrimaryHDU(final_data)
 	hdu.writeto(dataWritePath+"output_img_"+str(i)+".fits",overwrite=True) 
 	# !!!! --- if you want to replace the exisiting file -- replace the name with the same name as that of the existing file and also add overwrite=True
 	del hdu #to avoid garbage collection issues 
 
-if __name__ == '__main__':
 
-	pl = Pool(NUM_THREADS)
-	#pl.map(convolve_psf,range(0,NUM_FILES_TOTAL))
 
-	#if you don't want this just replace this with the previous line
-	#this next line does the same thing as the pool call above, but now with a progress bar
-	for _ in tqdm.tqdm(pl.imap_unordered(convolve_psf,range(0,NUM_FILES_TOTAL)), total=NUM_FILES_TOTAL):
-    		pass
+
+pl = Pool(NUM_THREADS)
+#pl.map(convolve_psf,range(0,NUM_FILES_TOTAL))
+
+#if you don't want this just replace this with the previous line
+#this next line does the same thing as the pool call above, but now with a progress bar
+for _ in tqdm.tqdm(pl.imap_unordered(psf_and_noise,range(0,NUM_FILES_TOTAL)), total=NUM_FILES_TOTAL):
+	pass
 
 
